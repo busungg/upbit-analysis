d8c7c6b46e200cb77b2b486c2d31134eaa5167e9
busungg <busungg@naver.com>
2020-12-06 15:08:54 +0900

    Init Project 1. http connection test (upbit open api) with httpclient

---
+++ Analysis/DataBot/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+// https://docs.microsoft.com/ko-kr/dotnet/csharp/tutorials/console-webapiclient
+
+namespace DataBot
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        private static async Task Call()
+        {
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var stringTask = client.GetStringAsync(@"https://api.upbit.com/v1/ticker?markets=KRW-BTC");
+
+            var msg = await stringTask;
+            Console.WriteLine(msg);
+        }
+
+        static async Task Main(string[] args)
+        {
+            await Call();
+        }
+    }
+}
